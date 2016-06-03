10f5d738798b124cb7a874d00a24927da2d499de
wangwei <ronawilliam@163.com>
2016-06-03 14:44:13 +0800

    bank limit

--- src/com/phb/puhuibao/entity/Bank.java
+++ src/com/phb/puhuibao/entity/Bank.java
@@ -3,7 +3,36 @@ package com.phb.puhuibao.entity;
 public class Bank {
 	private Integer bankId;
 	private String bankName;
+	private String bankIcon;
+	private Double orderLimit;//order_limit
+	private Double dayLimit;//day_limit
+	private Double monthLimit;//month_limit
+	 
 	
+	public String getBankIcon() {
+		return bankIcon;
+	}
+	public void setBankIcon(String bankIcon) {
+		this.bankIcon = bankIcon;
+	}
+	public Double getOrderLimit() {
+		return orderLimit;
+	}
+	public void setOrderLimit(Double orderLimit) {
+		this.orderLimit = orderLimit;
+	}
+	public Double getDayLimit() {
+		return dayLimit;
+	}
+	public void setDayLimit(Double dayLimit) {
+		this.dayLimit = dayLimit;
+	}
+	public Double getMonthLimit() {
+		return monthLimit;
+	}
+	public void setMonthLimit(Double monthLimit) {
+		this.monthLimit = monthLimit;
+	}
 	public Integer getBankId() {
 		return bankId;
 	}
--- src/com/phb/puhuibao/web/controller/BankController.java
+++ src/com/phb/puhuibao/web/controller/BankController.java
@@ -17,6 +17,7 @@ import com.phb.puhuibao.entity.Bank;
 @Controller
 @RequestMapping(value = "/bank")
 public class BankController extends BaseController<Bank, String> {
+	@Override
 	@Resource(name = "bankService")
 	public void setBaseService(IBaseService<Bank, String> baseService) {
 		super.setBaseService(baseService);
@@ -37,4 +38,24 @@ public class BankController extends BaseController<Bank, String> {
 		data.put("status", 1);
 		return data;
 	}
+	
+	/**
+	 * 银行列表
+	 * @return
+	 */
+	@RequestMapping(value="bankLimitation")
+	@ResponseBody
+	public Map<String, Object> bankLimitation() {
+		Map<String,Object> params = new HashMap<String,Object>();
+		List<Bank> result = this.getBaseService().findList(params);
+		Map<String, Object> data = new HashMap<String, Object>();
+		data.put("result", result);
+		data.put("message", "");
+		data.put("status", 1);
+		return data;
+	}
+	
+	
+ 
+	
 }
