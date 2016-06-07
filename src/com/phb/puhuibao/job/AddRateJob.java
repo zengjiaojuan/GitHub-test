7ba9c2f760fa5737e51d69204551aad5c92eaeb0
wangwei <ronawilliam@163.com>
2016-06-07 15:14:15 +0800

    jiaxijuan expire every day at 23:00

---
+++ src/com/phb/puhuibao/job/AddRateJob.java
@@ -0,0 +1,50 @@
+package com.phb.puhuibao.job;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import com.idp.pub.service.IBaseService;
+import com.phb.puhuibao.entity.UserAddrate;
+
+
+// 今天晚上11点把过期加息劵置为无效
+@Component  
+public class AddRateJob {
+ 
+	private static final Log log = LogFactory.getLog(AddRateJob.class);
+	@javax.annotation.Resource(name = "userAddrateService")
+	private IBaseService<UserAddrate, String> userAddrateService;
+	
+ 
+
+	@javax.annotation.Resource(name = "jdbcTemplate")
+	private JdbcTemplate jdbcTemplate;
+
+	 
+	@Scheduled(cron="0 0 23 * * ?")
+    public void process() {
+		try {
+			String sql = "select record_id from phb_muser_addrate t where   date(t.last_date) =  curdate() and t.rate_status =1";
+			List<Map<String, Object>> l = this.jdbcTemplate.queryForList(sql);
+			UserAddrate addrate = new UserAddrate();
+			 int recordid;
+			 int i=l.size()+1;
+			for (Map<String, Object> m : l) {
+				System.out.println(i + "to process");
+				recordid = Integer.parseInt( m.get("record_id").toString());
+				addrate.setRecordId(recordid);
+				addrate.setRateStatus(0);
+				userAddrateService.update(addrate);
+			} 
+		} catch (Exception e) {
+			log.error("加息劵job任务出错:"+e.getLocalizedMessage());
+			 
+		}
+	}
+}
