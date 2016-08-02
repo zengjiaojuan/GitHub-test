d2ba9499a0d6581a7eadde321b9704f5a504ad41
wangwei <ronawilliam@163.com>
2016-08-02 15:32:37 +0800

    无用代码

--- src/com/phb/puhuibao/common/Constants.java
+++ src/com/phb/puhuibao/common/Constants.java
@@ -11,35 +11,6 @@ public class Constants {
 	public static final String SERVICE_AGREEMENT_TEMPLATE = "/templates/puhuibao/serviceagreement.html";
 	public static final String ERROR_TEMPLATE = "/templates/puhuibao/error.html";
 	
-	public static final int hexagramCredit_userlevel = 10;//用户最高级别
-	public static final int hexagramCredit_usercomment = 200;//评价最高积分
-	public static final int hexagramCredit_authentication = 1;//是否认证
-	
-	public static final int hexagramAbility_abilitynumber=10;//
-	public static final int hexagramAbility_specialabilitynumber=2 ;// 
-	public static final int hexagramAbility_friendsnumber=200;// 
-	public static final int hexagramAbility_messioncomplitenumber=20;// 
-	 
-	
-	public static final int hexagramCharm_maxfannumber=200;//
-	public static final int hexagramCharm_maxfriendnumber=400 ;// 
-	public static final int hexagramCharm_maxphotonumber=10;// 
-	
-	
-	public static final int hexagramFame_validmessions=20;//
-	public static final int hexagramFame_sharetimes=200 ;// 
-	public static final int hexagramFame_evaluatescore=200;// 
-	public static final int hexagramFame_multipersonmession=20;
-	
-	public static final double hexagramFortune_depositvalue=4.5;//
-	public static final int hexagramFortune_messionpublish=2 ;// 
-	public static final int hexagramFortune_investvalue=3;// 
-
-	// 六芒星最大值 - lyb
-	public static final int GAMINO_ABILITY_SCORE = 10; // 能力
-	public static final int GAMINO_CHARM_SCORE = 10; // 魅力
-	public static final int GAMINO_REPUTATION_SCORE = 10; // 声望
-	public static final double GAMINO_CREDIT_SCORE = 10; // 信用
-	public static final double GAMINO_FORTUNE_SCORE = 10; // 财富 
+ 
 
 }
