f53098cbc4a1d26324c003acf7b9fc7b19736abc
wangwei <ronawilliam@163.com>
2016-08-16 14:24:40 +0800

    安全保障

--- src/com/phb/puhuibao/common/Constants.java
+++ src/com/phb/puhuibao/common/Constants.java
@@ -9,6 +9,7 @@ public class Constants {
 	public static final String AGREEMENT_TEMPLATE = "/templates/puhuibao/agreement.html";
 	public static final String MONTH_AGREEMENT_TEMPLATE = "/templates/puhuibao/monthAgreement.html";
 	public static final String SERVICE_AGREEMENT_TEMPLATE = "/templates/puhuibao/serviceagreement.html";
+	public static final String SCURET_ASSURANCE = "/templates/puhuibao/scuretassurance.html";
 	public static final String ERROR_TEMPLATE = "/templates/puhuibao/error.html";
 	
  
