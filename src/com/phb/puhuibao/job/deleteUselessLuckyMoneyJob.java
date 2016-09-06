4d8d66764afc6c4bc6896ada856675d1c8ea9659
yangjindi <244526004@qq.com>
2016-09-06 10:16:03 +0800

    每晚自动删除无效刮奖数据

--- src/com/phb/puhuibao/job/deleteUselessLuckyMoneyJob.java
+++ src/com/phb/puhuibao/job/deleteUselessLuckyMoneyJob.java
@@ -3,7 +3,6 @@ package com.phb.puhuibao.job;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 // 今天晚上10点把过期无效刮奖删除  减少垃圾数据
@@ -13,10 +12,10 @@ public class deleteUselessLuckyMoneyJob {
 	@javax.annotation.Resource(name = "jdbcTemplate")
 	private JdbcTemplate jdbcTemplate;
 	 
-	@Scheduled(cron="0 0 22 * * ?")
+
     public void process() {
 		try {
-			String sql = "delete from phb_muser_experience where status =0";
+			String sql = "delete from phb_muser_experience where experience_amount =0";
 			 this.jdbcTemplate.execute(sql);
 			 
 		} catch (Exception e) {
